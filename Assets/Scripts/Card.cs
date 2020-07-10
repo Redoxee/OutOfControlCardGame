0242c73a3d9309cd9d296e7ee499db7b9be20ebc
Anton Roy <antonmakesgames@gmail.com>
2020-07-11 01:50:11 +0200

    Initial project commit.

---
+++ Assets/Scripts/Card.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using Shapes;
+using UnityEngine;
+using TMPro;
+
+public enum Sigil
+{
+    Star,
+    Cross,
+    Serpent,
+    Heart,
+}
+
+public class Card : MonoBehaviour
+{
+    public int NumberValue;
+    public Sigil Sigil;
+
+    [SerializeField]
+    private TextMeshPro NumberLabel = null;
+    [SerializeField]
+    private TextMeshPro SigilLabel = null;
+
+    public Color BaseBorderColor = Color.black;
+    public Color HoverColor = Color.yellow;
+
+    [SerializeField]
+    private Shapes.ShapeRenderer borderRectangle = null;
+
+    private void OnMouseEnter()
+    {
+        this.borderRectangle.Color = this.HoverColor;
+    }
+
+    private void OnMouseExit()
+    {
+        this.borderRectangle.Color = this.BaseBorderColor;
+    }
+}
---
+++ Assets/Scripts/CardSlot.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardSlot : MonoBehaviour
+{
+    [SerializeField]
+    private Shapes.Rectangle Back = null;
+
+    [SerializeField]
+    private Shapes.Rectangle Border = null;
+
+    [SerializeField]
+    private Color baseBorderColor = Color.grey;
+    [SerializeField]
+    private Color hoverBorderColor = Color.grey;
+
+    private void OnMouseEnter()
+    {
+        this.Border.Color = this.hoverBorderColor;
+    }
+
+    private void OnMouseExit()
+    {
+        this.Border.Color = this.baseBorderColor;
+    }
+}
