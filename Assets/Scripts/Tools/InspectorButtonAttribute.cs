b71a67e182201a8cbae00129db770534fe42ecbc
Anton Roy <antonmakesgames@gmail.com>
2020-07-30 01:13:11 +0200

    Added a tool to automate the creation of card like object in the scene.

---
+++ Assets/Scripts/Tools/InspectorButtonAttribute.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+using System.Reflection;
+
+/// <summary>
+/// This attribute can only be applied to fields because its
+/// associated PropertyDrawer only operates on fields (either
+/// public or tagged with the [SerializeField] attribute) in
+/// the target MonoBehaviour.
+/// </summary>
+[System.AttributeUsage(System.AttributeTargets.Field)]
+public class InspectorButtonAttribute : PropertyAttribute
+{
+    public static float kDefaultButtonWidth = 80;
+
+    public readonly string MethodName;
+    public readonly string ButtonLabel;
+
+    private float _buttonWidth = kDefaultButtonWidth;
+    public float ButtonWidth
+    {
+        get { return _buttonWidth; }
+        set { _buttonWidth = value; }
+    }
+
+    public InspectorButtonAttribute(string MethodName, string buttonLabel = "")
+    {
+        this.MethodName = MethodName;
+        this.ButtonLabel = buttonLabel;
+    }
+}
+
+#if UNITY_EDITOR
+[CustomPropertyDrawer(typeof(InspectorButtonAttribute))]
+public class InspectorButtonPropertyDrawer : PropertyDrawer
+{
+    private MethodInfo _eventMethodInfo = null;
+
+    public override void OnGUI(Rect position, SerializedProperty prop, GUIContent label)
+    {
+        InspectorButtonAttribute inspectorButtonAttribute = (InspectorButtonAttribute)attribute;
+        Rect buttonRect = new Rect(position.x, position.y, position.width, UnityEditor.EditorGUIUtility.singleLineHeight);
+        if (GUI.Button(buttonRect, !string.IsNullOrEmpty(inspectorButtonAttribute.ButtonLabel) ? inspectorButtonAttribute.ButtonLabel : label.text))
+        {
+            System.Type eventOwnerType = prop.serializedObject.targetObject.GetType();
+            string eventName = inspectorButtonAttribute.MethodName;
+
+            if (_eventMethodInfo == null)
+                _eventMethodInfo = eventOwnerType.GetMethod(eventName, BindingFlags.Instance | BindingFlags.Static | BindingFlags.Public | BindingFlags.NonPublic);
+
+            if (_eventMethodInfo != null)
+                _eventMethodInfo.Invoke(prop.serializedObject.targetObject, null);
+            else
+                Debug.LogWarning(string.Format("InspectorButton: Unable to find method {0} in {1}", eventName, eventOwnerType));
+        }
+
+        position = new Rect(position.x, position.y + UnityEditor.EditorGUIUtility.singleLineHeight + UnityEditor.EditorGUIUtility.standardVerticalSpacing, position.width, UnityEditor.EditorGUIUtility.singleLineHeight);
+        EditorGUI.PropertyField(position, prop, label);
+    }
+
+    public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+    {
+        return base.GetPropertyHeight(property, label) + UnityEditor.EditorGUIUtility.singleLineHeight + UnityEditor.EditorGUIUtility.standardVerticalSpacing;
+    }
+}
+#endif
---
+++ Assets/Scripts/Tools/Rectangler.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Shapes;
+
+public class Rectangler : MonoBehaviour
+{
+    [InspectorButton("SetupRectangle", "Setup rectangle")]
+    [SerializeField]
+    private Rectangle back = null;
+    [SerializeField]
+    private Vector2 backSize = Vector2.one;
+    [SerializeField]
+    private Color backColor = Color.white;
+    [SerializeField]
+    private Rectangle border = null;
+    [SerializeField]
+    private float borderThickness = 1;
+    [SerializeField]
+    private Color borderColor = Color.white;
+    [SerializeField]
+    private Rectangle rim = null;
+    [SerializeField]
+    private float rimThickness = .1f;
+    [SerializeField]
+    private Color rimColor = Color.black;
+
+    [SerializeField]
+    private float cornerRadius = 0;
+
+    private void SetupRectangle()
+    {
+        int childCount = this.transform.childCount;
+        for (int index = 0; index < childCount; ++index)
+        {
+            Transform child = this.transform.GetChild(index);
+            Rectangle childRectangle = child.GetComponent<Rectangle>();
+            if (this.back == null && child.name == "Back" && childRectangle != null)
+            {
+                this.back = childRectangle;
+            }
+
+            if (this.border == null && child.name == "Border" && childRectangle != null)
+            {
+                this.border = childRectangle;
+            }
+
+            if (this.rim == null && child.name == "Rim" && childRectangle != null)
+            {
+                this.rim = childRectangle;
+            }
+        }
+
+        if (this.back == null)
+        {
+            GameObject backObject = new GameObject("Back");
+            backObject.transform.SetParent(this.transform, false);
+            this.back = backObject.AddComponent(typeof(Shapes.Rectangle)) as Rectangle;
+            this.back.Type = Rectangle.RectangleType.RoundedSolid;
+        }
+
+        if (this.border == null)
+        {
+            GameObject borderObject = new GameObject("Border");
+            borderObject.transform.SetParent(this.transform, false);
+            this.border = borderObject.AddComponent(typeof(Shapes.Rectangle)) as Rectangle;
+            this.border.Type = Rectangle.RectangleType.RoundedHollow;
+            borderObject.transform.localPosition = new Vector3(0, 0, .25f);
+        }
+
+        if (this.rim == null)
+        {
+            GameObject rimObject = new GameObject("Rim");
+            rimObject.transform.SetParent(this.transform, false);
+            this.rim = rimObject.AddComponent(typeof(Shapes.Rectangle)) as Rectangle;
+            this.rim.Type = Rectangle.RectangleType.RoundedHollow;
+            rimObject.transform.localPosition = new Vector3(0, 0, .5f);
+        }
+
+        this.back.CornerRadius = this.cornerRadius;
+        this.border.CornerRadius = this.cornerRadius;
+        this.rim.CornerRadius = this.cornerRadius;
+
+        this.back.Color = this.backColor;
+        this.border.Color = borderColor;
+        this.rim.Color = this.rimColor;
+
+        this.back.Width = this.backSize.x;
+        this.back.Height = this.backSize.y;
+
+        this.border.Width = this.back.Width + this.borderThickness;
+        this.border.Height = this.back.Height + this.borderThickness;
+        this.border.Thickness = this.borderThickness;
+
+        this.rim.Width = this.border.Width + this.rimThickness;
+        this.rim.Height = this.border.Height + this.rimThickness;
+        this.rim.Thickness = this.rimThickness;
+    }
+}
