35d9123f6dd78cd711addbbb9b38835663c71f16
Anton Roy <antonmakesgames@gmail.com>
2020-07-31 00:35:23 +0200

    Tweaked Rules appearance

--- Assets/Scripts/Game/Rule.cs
+++ Assets/Scripts/Game/Rule.cs
@@ -11,14 +11,10 @@ public class Rule : BorderComponent
     private SpriteRenderer[] IconRenderer = null;
     [SerializeField]
     private float IconSeparation = 1;
-
-    [SerializeField]
-    private TextMeshPro label = null;
-
+    
     public void SetRule(RuleDefinition rule)
     {
         this.Data = rule;
-        this.label.text = this.Data.ToString();
 
         Sprite[] sprites = rule.GetRuleSprites();
 
@@ -28,11 +24,11 @@ public class Rule : BorderComponent
         {
             if (index >= sprites.Length)
             {
-                this.IconRenderer[index].enabled = false;
+                this.IconRenderer[index].gameObject.SetActive(false);
                 continue;
             }
 
-            this.IconRenderer[index].enabled = true;
+            this.IconRenderer[index].gameObject.SetActive(true);
             this.IconRenderer[index].sprite = sprites[index];
             Vector3 position = this.IconRenderer[index].transform.localPosition;
             position.x = -halfWidth + (index * this.IconSeparation);
