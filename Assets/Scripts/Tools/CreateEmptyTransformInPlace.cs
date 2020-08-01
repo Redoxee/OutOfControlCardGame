e3392423b486409039dfc31ad6dc3f505d383003
Anton Roy <antonmakesgames@gmail.com>
2020-08-01 19:19:05 +0200

    Added a tool to make anchors.

---
+++ Assets/Scripts/Tools/CreateEmptyTransformInPlace.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public static class GameObjectUtils
+{
+    [MenuItem("GameObject/DuplicateEmptyTransform", true, 10)]
+    public static bool ValidateSelectedGameObjectInScene()
+    {
+        GameObject source = UnityEditor.Selection.activeGameObject;
+        if (source == null)
+        {
+            return false;
+        }
+        
+        if(source.scene == null)
+        {
+            return false;
+        }
+
+        return true;
+    }
+
+    [MenuItem("GameObject/DuplicateEmptyTransform", false, 10)]
+    public static GameObject DuplicateObjectTransformInPlace()
+    {
+        GameObject source = UnityEditor.Selection.activeGameObject;
+        if (source == null)
+        {
+            return null;
+        }
+
+        GameObject result = new GameObject();
+        if (source.transform.parent != null)
+        {
+            result.transform.parent = source.transform.parent;
+        }
+
+        result.transform.position = source.transform.position;
+        result.name = source.name;
+
+        UnityEditor.Selection.SetActiveObjectWithContext(result, null);
+        return result;
+    }
+}
